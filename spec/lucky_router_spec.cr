3dab7f3b04558c6bd4969bedc39996c3e1645cdf
Paul Smith <paulcsmith0218@gmail.com>
2017-09-14 21:31:07 -0400

    Add param handling

--- spec/lucky_router_spec.cr
+++ spec/lucky_router_spec.cr
@@ -12,15 +12,23 @@ describe LuckyRouter do
     router.add("get/users/:id/edit", :edit)
     router.add("get/users/:id/new", :new)
 
-    1000.times do
-      router.match!("get/users").payload.should eq :index
-      router.match!("post/users").payload.should eq :create
-      router.match!("get/users/1").payload.should eq :show
-      router.match!("delete/users/1").payload.should eq :delete
-      router.match!("put/users/1").payload.should eq :update
-      router.match!("get/users/1/edit").payload.should eq :edit
-      router.match!("get/users/1/new").payload.should eq :new
-    end
+    router.match!("get/users").payload.should eq :index
+    router.match!("post/users").payload.should eq :create
+    router.match!("get/users/1").payload.should eq :show
+    router.match!("delete/users/1").payload.should eq :delete
+    router.match!("put/users/1").payload.should eq :update
+    router.match!("get/users/1/edit").payload.should eq :edit
+    router.match!("get/users/1/new").payload.should eq :new
+  end
+
+  it "gets params" do
+    router = LuckyRouter::Matcher(Symbol).new
+    router.add("get/users/:user_id/tasks/:id", :show)
+
+    router.match!("get/users/user_param/tasks/task_param").params.should eq({
+      "user_id" => "user_param",
+      "id" => "task_param",
+    })
   end
 
   pending "handles conflicting routes" do
--- src/lucky_router/matcher.cr
+++ src/lucky_router/matcher.cr
@@ -13,7 +13,7 @@ class LuckyRouter::Matcher(T)
   def match!(path_to_match : String)
     parts_to_match = path_to_match.split("/")
     if route = routes.find(&.match?(parts_to_match))
-      route
+      MatchedRoute.new(route, parts_to_match)
     else
       raise "No matching route found for: #{path_to_match}"
     end
@@ -27,9 +27,29 @@ class LuckyRouter::Matcher(T)
     end
   end
 
+  class MatchedRoute(T)
+    private getter route, parts_to_match
+
+    def initialize(@route : Route(T), @parts_to_match : Array(String))
+    end
+
+    def payload
+      route.payload
+    end
+
+    def params : Hash(String, String)
+      params_hash = {} of String => String
+      route.named_parts_with_indices.each do |index, part_name|
+        params_hash[part_name] = parts_to_match[index]
+      end
+      params_hash
+    end
+  end
+
   struct Route(T)
     getter path, payload
     @path_parts : Array(String)?
+    @named_parts_with_indices : Hash(Int32, String)?
 
     def initialize(@path : String, @payload : T)
     end
@@ -38,7 +58,7 @@ class LuckyRouter::Matcher(T)
       all_parts_match?(parts_to_match) && parts_to_match.size == path_parts.size
     end
 
-    private def path_parts
+    def path_parts
       @path_parts ||= path.split("/")
     end
 
@@ -46,6 +66,13 @@ class LuckyRouter::Matcher(T)
       parts_to_match.each_with_index.all? do |part, index|
         path_part = path_parts[index]?
         path_part == part || path_part.try(&.starts_with?(":"))
+      end
+    end
+
+    def named_parts_with_indices
+      @named_parts_with_indices ||= path_parts.each_with_index.reduce({} of Int32 => String) do |named_part_hash, (part, index)|
+        named_part_hash[index] = part.gsub(":", "") if part.starts_with?(":")
+        named_part_hash
       end
     end
   end
