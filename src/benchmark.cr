b7280b3ce6efe9d69b33b9d1d8c8e76c1ca0e84a
Paul Smith <paulcsmith0218@gmail.com>
2017-09-21 19:36:08 -0400

    Update benchmark

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -4,20 +4,27 @@ time = Time.now
 
 router = LuckyRouter::Matcher(Symbol).new
 
-router.add("post/users", :index)
-router.add("get/users/:id", :show)
-router.add("delete/users/:id", :delete)
-router.add("put/users/:id", :update)
-router.add("get/users/:id/edit", :edit)
-router.add("get/users/:id/new", :new)
+1000.times do
+  router.add("put", "/users/:id", :update)
+  router.add("get", "/users/:id/edit", :edit)
+  router.add("get", "/users/:id/new", :new)
+end
+
+router.add("get", "/users", :index)
+router.add("post", "/users", :create)
+router.add("get", "/users/:id", :show)
+router.add("delete", "/users/:id", :delete)
+router.add("put", "/users/:id", :update)
+router.add("get", "/users/:id/edit", :edit)
+router.add("get", "/users/:id/new", :new)
 
 1000.times do
-  router.match!("post/users")
-  router.match!("get/users/1")
-  router.match!("delete/users/1")
-  router.match!("put/users/1")
-  router.match!("get/users/1/edit")
-  router.match!("get/users/1/new")
+  router.match!("post", "/users")
+  router.match!("get", "/users/1")
+  router.match!("delete", "/users/1")
+  router.match!("put", "/users/1")
+  router.match!("get", "/users/1/edit")
+  router.match!("get", "/users/1/new")
 end
 
 elapsed = Time.now - time
