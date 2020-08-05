c0b0cc867061d52765be89d617353f45ea5052aa
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-08-05 15:59:42 -0500

    temp

--- src/lucky_router/part_processor.cr
+++ src/lucky_router/part_processor.cr
@@ -11,56 +11,27 @@ class LuckyRouter::PartProcessor(T)
 
   def run
     unless parts.empty?
-      process_parts
+      current_part = parts.first
+      next_parts = parts.skip(1)
+
+      if current_part.starts_with?(":")
+        dynamic_part = fragment.dynamic_part ||= Fragment::DynamicFragment.new(
+          name: current_part.gsub(":", ""),
+          fragment: Fragment(T).new
+        )
+        dynamic_part.fragment.process_parts(next_parts, method, payload)
+
+        if next_parts.empty?
+          dynamic_part.fragment.method_to_payload[method] = payload
+        end
+      else
+        new_fragment = fragment.static_parts[current_part] ||= Fragment(T).new
+        new_fragment.process_parts(next_parts, method, payload)
+
+        if next_parts.empty?
+          new_fragment.method_to_payload[method] = payload
+        end
+      end
     end
-  end
-
-  private def process_parts
-    if dynamic_part?
-      add_dynamic_part
-    else
-      add_static_part
-    end
-  end
-
-  private def dynamic_part?
-    current_part.starts_with?(":")
-  end
-
-  private def current_part
-    parts.first
-  end
-
-  private def next_parts
-    parts.skip(1)
-  end
-
-  private def add_dynamic_part
-    fragment.dynamic_part ||= Fragment::DynamicFragment.new(
-      name: current_part.gsub(":", ""),
-      fragment: Fragment(T).new
-    )
-    fragment.dynamic_part.not_nil!.fragment.process_parts(next_parts, method, payload)
-
-    if on_last_part?
-      add_payload_to_dynamic_part
-    end
-  end
-
-  def on_last_part?
-    next_parts.empty?
-  end
-
-  private def add_static_part
-    fragment.static_parts[current_part] ||= Fragment(T).new
-    fragment.static_parts[current_part].process_parts(next_parts, method, payload)
-
-    if next_parts.empty?
-      fragment.static_parts[current_part].method_to_payload[method] = payload
-    end
-  end
-
-  private def add_payload_to_dynamic_part
-    fragment.dynamic_part.not_nil!.fragment.method_to_payload[method] = payload
   end
 end
