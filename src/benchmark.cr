f36d5af017798cf0bc44800efe2bc72ee86717cf
Russ Smith <russ@bashme.org>
2025-06-12 07:28:10 -0700

    Benchmark runs on github actions with PR commenting.
    This github action will be run on PR's to compare benchmark times to the
    base branch. After the comparison, it will add post a comment to the PR
    with results. The comment contains the runtime information as well as
    the comparison results, negative or positive.
    
    The github action does require a `GH_TOKEN` action secret to be setup
    first. The token needs the following permissions on this repo:
    
    - Read access to metadata
    - Read and Write access to code and pull requests

--- src/benchmark.cr
+++ src/benchmark.cr
@@ -1,3 +1,4 @@
+require "benchmark"
 require "./lucky_router"
 
 router = LuckyRouter::Matcher(Symbol).new
@@ -16,31 +17,13 @@ router.add("put", "/users/:id", :update)
 router.add("get", "/users/:id/edit", :edit)
 router.add("get", "/users/:id/new", :new)
 
-time = Time.now
-
-1000.times do
-  router.match!("post", "/users")
-  router.match!("get", "/users/1")
-  router.match!("delete", "/users/1")
-  router.match!("put", "/users/1")
-  router.match!("get", "/users/1/edit")
-  router.match!("get", "/users/1/new")
-end
-
-elapsed = Time.now - time
-elapsed_text = elapsed_text(elapsed)
-
-puts elapsed_text
-
-private def elapsed_text(elapsed)
-  minutes = elapsed.total_minutes
-  return "#{minutes.round(2)}m" if minutes >= 1
-
-  seconds = elapsed.total_seconds
-  return "#{seconds.round(2)}s" if seconds >= 1
-
-  millis = elapsed.total_milliseconds
-  return "#{millis.round(2)}ms" if millis >= 1
-
-  "#{(millis * 1000).round(2)}µs"
+Benchmark.ips do |x|
+  x.report("LuckyRouter match!") do
+    router.match!("post", "/users")
+    router.match!("get", "/users/1")
+    router.match!("delete", "/users/1")
+    router.match!("put", "/users/1")
+    router.match!("get", "/users/1/edit")
+    router.match!("get", "/users/1/new")
+  end
 end
